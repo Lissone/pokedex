75a86bf2f244b3775beaba6241eb3bedc4983d89
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-07-02 16:28:57 -0300

    global styles and assets

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Home - Pokedex</title>
       </Head>
 
       <Container>
--- packages/web/src/styles/global.ts
+++ packages/web/src/styles/global.ts
@@ -8,8 +8,20 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :root {
-    --background: #494949;
-    --light: #f8f9fa;
+    --white: #FAFAFA;
+
+    --blue-200: #A1B2CD;
+    --blue-900: #282D34;
+
+    --primary: #654BB1;
+
+    --gray-600: #6D6D6D;
+    --gray-700: #3E3E3E;
+    --gray-750: #444444;
+    --gray-800: #3E3E3E;
+    --gray-900: #171717;
+
+    --black: #000000;
   }
 
   @media (max-width: 1080px) {
@@ -26,25 +38,29 @@ export const GlobalStyle = createGlobalStyle`
 
 
   body {
-    background: var(--background);
+    background: var(--gray-700);
   }
 
   body, input, textarea, button {
-    font: 500 1rem Inter, sans-serif;
-    color: var(--light);
+    font: 400 1rem Poppins, sans-serif;
+    color: var(--white);
   }
 
-  h1, h2, h3, h4, h5, h6 {
-    font-weight: 600;
-    color: var(--light);
+h3, h4, h5, h6 {
+    font-weight: 500;
+    color: var(--white);
   }
 
   h1 {
+    font-weight: 700;
     font-size: 2rem;
+    color: var(--white);
   }
 
   h2 {
+    font-weight: 700;
     font-size: 1.5rem;
+    color: var(--white);
   }
 
   button {
