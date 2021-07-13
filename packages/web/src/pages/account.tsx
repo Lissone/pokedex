cdedfb6c6b98136a3fe40e449cebf533f3131022
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-07-13 19:33:09 -0300

    ordering pokemons on the account page

--- packages/web/src/pages/account.tsx
+++ packages/web/src/pages/account.tsx
@@ -94,6 +94,15 @@ export default function Account() {
 
                     return null
                   })
+                  .sort((a, b) => {
+                    if (a.id > b.id) {
+                      return 1
+                    }
+                    if (a.id < b.id) {
+                      return -1
+                    }
+                    return 0
+                  })
                   .map(pokemon => (
                     <PokemonCard
                       key={pokemon.id}
